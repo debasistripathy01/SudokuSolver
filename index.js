1a61b268afe0ce86efd14adb05c35f097b1c07d8
debasistripathy1 <debasistripathy04@gmail.com>
2022-09-08 19:40:39 +0530

    4

--- index.js
+++ index.js
@@ -1,58 +1,74 @@
-const puzzle = document.getElementById("puzzle");
-const solveButton = document.getElementById("solve-button");
-const valueOfIn =[];
-const square = 81;
+const puzzleBoard = document.querySelector('#puzzle')
+const solveButton = document.querySelector('#solve-button')
+const explainerDisplay = document.querySelector('#explainer')
+const squares = 81
+let submission = []
 
-for(let i=0; i < square; i++){
-    const inputElement =  document.createElement("Input");
-    inputElement.setAttribute("type", "number");
-    inputElement.setAttribute("min","0");
-    inputElement.setAttribute("max","9");
+for (let i = 0; i < squares; i++) {
+  const inputElement = document.createElement('input')
+  inputElement.setAttribute('type', 'number')
+  inputElement.setAttribute('min', '0')
+  inputElement.setAttribute('max', '9')
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53)) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+  ) {
+    inputElement.classList.add('odd-section')
+  }
+  puzzleBoard.appendChild(inputElement)
+}
 
-    puzzle.appendChild(inputElement);
+const joinValues = () => {
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach(input => {
+    if (input.value) {
+      submission.push(input.value)
+    } else {
+      submission.push('.')
+    }
+  })
+  console.log(submission.join(''))
+}
+
+const populateValues = (isSolvable, solution) => {
+  const inputs = document.querySelectorAll('input')
+  if (isSolvable && solution) {
+    inputs.forEach((input, i) => {
+      input.value = solution[i]
+    })
+    explainerDisplay.innerHTML = 'This is the answer'
+    } else {
+    explainerDisplay.innerHTML = 'This is not solvable'
+    }
+
+}
+
+const solve = async () => {
+  joinValues()
+  const data = { numbers: submission.join('') }
+
+  fetch('http://localhost:8000/solve', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data)
+    populateValues(data.solvable, data.solution)
+    submission = []
+  })
+  .catch((error) => {
+    console.error('Error:', error)
+  })
+
 }
 
 
-
-
-const joinValues  = () =>{
-    const inputs = document.querySelectorAll("input");
-    
-    inputs.forEach(input=>{
-        if(input.value){
-            valueOfIn.push(input.value);
-        }
-        else{
-            valueOfIn.push(".")
-        }
-    });
-    console.log(valueOfIn);
-};
-
-
-// import axios from "axios";
-
-
-let solve =()=>{
-    // let axios = require('axios');
-    joinValues();
-    const axios = require("axios");
-
-                const options = {
-                method: 'POST',
-                url: 'https://solve-sudoku.p.rapidapi.com/',
-                headers: {
-                    'content-type': 'application/json',
-                    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-                    'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
-                },
-                data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
-                };
-
-                axios.request(options).then(function (response) {
-                    console.log(response.data);
-                }).catch(function (error) {
-                    console.error(error);
-                });
-};
-solveButton.addEventListener("click", solve);
+solveButton.addEventListener('click', solve)
